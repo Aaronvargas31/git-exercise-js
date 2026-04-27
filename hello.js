bfb5c3d4020f4d553fd6ed2319baae81f493a0a6
Aaron Vargas <aaronvargas@LAPTOP-2V0OJP9H.oc.edu>
2026-04-27 18:58:14 -0500

    Adds greet function and dev changes

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
 console.log("Hello, World! This is my first Git commit.");
+function greet(name) {
+ return `Hello, ${name}!`;
+}
+console.log(greet("Student"));
+console.log("Working on the dev branch!");

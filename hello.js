d3c76d2edb365f0cb1a101ab41e4ac2136111978
Aaron Vargas <aaronvargas@LAPTOP-2V0OJP9H.oc.edu>
2026-04-27 19:48:55 -0500

    Initial commit: Add hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,2 @@
 console.log("Hello, World! This is my first Git commit.");
-function greet(name) {
- return `Hello, ${name}!`;
-}
-console.log(greet("Student"));
-console.log("Working on the dev branch!");
 
